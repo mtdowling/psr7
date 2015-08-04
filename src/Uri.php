4cc9374dbdd8f831c849070d094a8b7518b83424
Kirill Konshin <kirill.konshin@gmail.com>
2015-08-04 00:23:52 -0700

    PHP 5.3 Compatibility

--- src/Uri.php
+++ src/Uri.php
@@ -11,14 +11,14 @@ use Psr\Http\Message\UriInterface;
  */
 class Uri implements UriInterface
 {
-    private static $schemes = [
+    private static $schemes = array(
         'http'  => 80,
         'https' => 443,
-    ];
+    );
 
     private static $charUnreserved = 'a-zA-Z0-9_\-\.~';
     private static $charSubDelims = '!\$&\'\(\)\*\+,;=';
-    private static $replaceQuery = ['=' => '%3D', '&' => '%26'];
+    private static $replaceQuery = array('=' => '%3D', '&' => '%26');
 
     /** @var string Uri scheme. */
     private $scheme = '';
@@ -76,14 +76,14 @@ class Uri implements UriInterface
      */
     public static function removeDotSegments($path)
     {
-        static $noopPaths = ['' => true, '/' => true, '*' => true];
-        static $ignoreSegments = ['.' => true, '..' => true];
+        static $noopPaths = array('' => true, '/' => true, '*' => true);
+        static $ignoreSegments = array('.' => true, '..' => true);
 
         if (isset($noopPaths[$path])) {
             return $path;
         }
 
-        $results = [];
+        $results = array();
         $segments = explode('/', $path);
         foreach ($segments as $segment) {
             if ($segment == '..') {
@@ -124,23 +124,23 @@ class Uri implements UriInterface
         }
 
         if ($rel instanceof UriInterface) {
-            $relParts = [
+            $relParts = array(
                 'scheme'   => $rel->getScheme(),
                 'host'     => $rel->getHost(),
                 'port'     => $rel->getPort(),
                 'path'     => $rel->getPath(),
                 'query'    => $rel->getQuery(),
                 'fragment' => $rel->getFragment()
-            ];
+            );
         } else {
-            $relParts = parse_url($rel) + [
+            $relParts = parse_url($rel) + array(
                 'scheme'   => '',
                 'host'     => '',
                 'port'     => '',
                 'path'     => '',
                 'query'    => '',
                 'fragment' => ''
-            ];
+                );
         }
 
         if (!empty($relParts['scheme']) && !empty($relParts['host'])) {
@@ -149,14 +149,14 @@ class Uri implements UriInterface
                 : self::fromParts($relParts);
         }
 
-        $parts = [
+        $parts = array(
             'scheme'   => $base->getScheme(),
             'host'     => $base->getHost(),
             'port'     => $base->getPort(),
             'path'     => $base->getPath(),
             'query'    => $base->getQuery(),
             'fragment' => $base->getFragment()
-        ];
+        );
 
         if (!empty($relParts['host'])) {
             $parts['host'] = $relParts['host'];
@@ -208,9 +208,10 @@ class Uri implements UriInterface
             return $uri;
         }
 
-        $result = [];
+        $result = array();
         foreach (explode('&', $current) as $part) {
-            if (explode('=', $part)[0] !== $key) {
+            $subParts = explode('=', $part);
+            if ($subParts[0] !== $key) {
                 $result[] = $part;
             };
         }
@@ -238,11 +239,12 @@ class Uri implements UriInterface
         $key = strtr($key, self::$replaceQuery);
 
         if (!$current) {
-            $result = [];
+            $result = array();
         } else {
-            $result = [];
+            $result = array();
             foreach (explode('&', $current) as $part) {
-                if (explode('=', $part)[0] !== $key) {
+                $subParts = explode('=', $part);
+                if ($subParts[0] !== $key) {
                     $result[] = $part;
                 };
             }
@@ -576,7 +578,7 @@ class Uri implements UriInterface
     {
         return preg_replace_callback(
             '/(?:[^' . self::$charUnreserved . self::$charSubDelims . ':@\/%]+|%(?![A-Fa-f0-9]{2}))/',
-            [$this, 'rawurlencodeMatchZero'],
+            array($this, 'rawurlencodeMatchZero'),
             $path
         );
     }
@@ -592,7 +594,7 @@ class Uri implements UriInterface
     {
         return preg_replace_callback(
             '/(?:[^' . self::$charUnreserved . self::$charSubDelims . '%:@\/\?]+|%(?![A-Fa-f0-9]{2}))/',
-            [$this, 'rawurlencodeMatchZero'],
+            array($this, 'rawurlencodeMatchZero'),
             $str
         );
     }
