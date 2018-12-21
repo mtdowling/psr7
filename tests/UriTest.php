b8288607ac93ece6069a178668773891834aa2f5
Cédric Belin <cedric@belin.io>
2018-12-21 14:26:31 +0100

    Adds a unit test for special chars in the `userInfo` component

--- tests/UriTest.php
+++ tests/UriTest.php
@@ -680,6 +680,13 @@ class UriTest extends BaseTest
             new ExtendedUriTest('http://h:9/')
         );
     }
+
+    public function testSpecialCharsOfUserInfo()
+    {
+        // The `userInfo` must always be URL-encoded.
+        $uri = (new Uri)->withUserInfo('foo@bar.com', 'pass#word');
+        $this->assertSame('foo%40bar.com:pass%23word', $uri->getUserInfo());
+    }
 }
 
 class ExtendedUriTest extends Uri
