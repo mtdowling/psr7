e99d85357c1408c5666a8885852c7e7314b8d209
Nyholm <tobias.nyholm@gmail.com>
2022-03-13 16:34:54 +0100

    Dont put the full file body as filename

--- src/MultipartStream.php
+++ src/MultipartStream.php
@@ -89,7 +89,8 @@ final class MultipartStream implements StreamInterface
 
         if (empty($element['filename'])) {
             $uri = $element['contents']->getMetadata('uri');
-            if ($uri && \is_string($uri) && \substr($uri, 0, 6) !== 'php://') {
+            $prefix = \substr($uri, 0, 6);
+            if ($uri && \is_string($uri) && !in_array($prefix, ['data:/', 'php://'])) {
                 $element['filename'] = $uri;
             }
         }
