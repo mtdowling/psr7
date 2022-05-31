8ce2f7c94c99f103ad1992d1002320e4a71384bb
Alexis Montenegro <AlexanderZon@users.noreply.github.com>
2022-05-31 17:27:52 -0300

    Throw and exception where multipart data is wrong
    I spend a lot of time searching for an error that was only a misformatted data in the multipart request option, and the server just die without give any information about of error. I think that an exception would be useful for other people that is as absent-minded as me.

--- src/MultipartStream.php
+++ src/MultipartStream.php
@@ -68,6 +68,7 @@ final class MultipartStream implements StreamInterface
         $stream = new AppendStream();
 
         foreach ($elements as $element) {
+            if(!is_array($element)) throw new \UnexpectedValueException("An array is required");
             $this->addElement($stream, $element);
         }
 
