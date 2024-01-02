ec653c0a950afa3394c04bdfc865f1bb6b213a6c
Noé Fleury <23384755+noefleury@users.noreply.github.com>
2024-01-02 22:36:39 +0100

    Cross origin check can now optionaly allow protocol upgrade to https

--- src/UriComparator.php
+++ src/UriComparator.php
@@ -15,19 +15,19 @@ final class UriComparator
 {
     /**
      * Determines if a modified URL should be considered cross-origin with
-     * respect to an original URL.
+     * respect to an original URL. Optionaly, we can allow http to https upgrade on base port.
      */
-    public static function isCrossOrigin(UriInterface $original, UriInterface $modified): bool
+    public static function isCrossOrigin(UriInterface $original, UriInterface $modified, bool $allowProtocolUpgrade = false): bool
     {
         if (\strcasecmp($original->getHost(), $modified->getHost()) !== 0) {
             return true;
         }
 
-        if ($original->getScheme() !== $modified->getScheme()) {
+        if ($original->getScheme() !== $modified->getScheme() && (!$allowProtocolUpgrade || $original->getScheme() === 'https')) {
             return true;
         }
 
-        if (self::computePort($original) !== self::computePort($modified)) {
+        if (self::computePort($original) !== self::computePort($modified) && (!$allowProtocolUpgrade || self::computePort($modified) !== 443)) {
             return true;
         }
 
--- tests/UriComparatorTest.php
+++ tests/UriComparatorTest.php
@@ -18,7 +18,15 @@ class UriComparatorTest extends TestCase
      */
     public function testIsCrossOrigin(string $original, string $modified, bool $expected): void
     {
-        self::assertSame($expected, UriComparator::isCrossOrigin(new Uri($original), new Uri($modified)));
+        self::assertSame($expected, UriComparator::isCrossOrigin(new Uri($original), new Uri($modified), false));
+    }
+
+    /**
+     * @dataProvider getCrossOriginAllowProtocolUpgradeExamples
+     */
+    public function testIsCrossOriginAllowProtocolUpgrade(string $original, string $modified, bool $expected): void
+    {
+        self::assertSame($expected, UriComparator::isCrossOrigin(new Uri($original), new Uri($modified), true));
     }
 
     public function getCrossOriginExamples(): array
@@ -32,6 +40,34 @@ class UriComparatorTest extends TestCase
             ['http://example.com/123', 'http://www.example.com/', true],
             ['http://example.com/123', 'http://example.com:81/', true],
             ['http://example.com:80/123', 'http://example.com:81/', true],
+            ['http://example.com:80/123', 'https://example.com:443/123', true],
+            ['http://example.com:81/123', 'https://example.com:443/123', true],
+            ['http://example.com/123', 'https://example.com/', true],
+            ['https://example.com/123', 'https://example.com/', false],
+            ['https://example.com/123', 'https://example.com:443/', false],
+            ['https://example.com:443/123', 'https://example.com/', false],
+            ['https://example.com:443/123', 'https://example.com:443/', false],
+            ['https://example.com/123', 'http://example.com/', true],
+            ['https://example.com/123', 'https://www.example.com/', true],
+            ['https://example.com/123', 'https://example.com:444/', true],
+            ['https://example.com:443/123', 'https://example.com:444/', true],
+        ];
+    }
+
+    public function getCrossOriginAllowProtocolUpgradeExamples(): array
+    {
+        return [
+            ['http://example.com/123', 'http://example.com/', false],
+            ['http://example.com/123', 'http://example.com:80/', false],
+            ['http://example.com:80/123', 'http://example.com/', false],
+            ['http://example.com:80/123', 'http://example.com:80/', false],
+            ['http://example.com/123', 'https://example.com/', false],
+            ['http://example.com/123', 'http://www.example.com/', true],
+            ['http://example.com/123', 'http://example.com:81/', true],
+            ['http://example.com:80/123', 'http://example.com:81/', true],
+            ['http://example.com:80/123', 'https://example.com:443/123', false],
+            ['http://example.com:81/123', 'https://example.com:443/123', false],
+            ['http://example.com/123', 'https://example.com/', false],
             ['https://example.com/123', 'https://example.com/', false],
             ['https://example.com/123', 'https://example.com:443/', false],
             ['https://example.com:443/123', 'https://example.com/', false],
